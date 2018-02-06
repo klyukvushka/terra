b9976e3cc459e99a2960e48333dcc1f51652b775
Kate <katyalex103@gmail.com>
2018-02-06 20:59:32 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var imagemin = require('gulp-imagemin');
+ 
+gulp.task('sass', function () {
+  return gulp.src('src/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task("images", function (){
+	gulp.src('src/images/**/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/images'))
+});
+
+
+gulp.task('watch', ['sass', 'images'], function() {
+    gulp.watch(['src/styles/**/*.scss'], ['sass']);
+    gulp.watch(['src/images/**/*'], ['images']);
+});
+
+gulp.task("default", ['watch']); 
